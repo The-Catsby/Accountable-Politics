7d403adbdfc936f548394f768c1f69dc643b31de
Alex Rappa <rappab@oregonstate.edu>
2016-06-03 11:54:05 -0500

    reverted models.py

--- models.py
+++ models.py
@@ -16,7 +16,7 @@ db_proxy = Proxy()
 if 'HEROKU' in os.environ:
     urllib.parse.uses_netloc.append('postgres')
     url = urlparse(os.environ["DATABASE_URL"])
-    db = psycopg2.connect(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
+    db = PostgresqlDatabase(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
     db_proxy.initialize(db)
 else:
     db = SqliteDatabase('accountable.db')
